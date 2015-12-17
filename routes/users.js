4feb88a2be703cbb61997c78e4e0d225afe4c608
joeldeval <jooelvaldivi@gmail.com>
2015-12-17 16:42:45 -0600

    example finish

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -6,9 +7,31 @@ var router = express.Router();
  */
 router.get('/userlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('userslist');
+    var collection = db.get('userlist');
     collection.find({},{},function(e,docs){
         res.json(docs);
+    });
+});
+
+router.post('/adduser', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deleteuser/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
     });
 });
 
